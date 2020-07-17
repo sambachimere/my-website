f4b9866268fcc3171bdb8268dca452438a5f08a9
Samba Diaw <sambachimerediaw@gmail.com>
2020-07-17 11:24:54 +0200

    Finish Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,11 +5,11 @@ import './Sidebar.css';
 const Sidebar = () => {
   return (
     <div className="Sidebar">
-      <div>
-        <h3 className="Sidebar-nav">About<span className="Sidebar-period">.</span></h3>
-        <h3 className="Sidebar-nav">Experience<span className="Sidebar-period">.</span></h3>
-        <h3 className="Sidebar-nav">Portfolio<span className="Sidebar-period">.</span></h3>
-        <h3 className="Sidebar-nav">Contact<span className="Sidebar-period">.</span></h3>
+      <div className="Sidebar-nav">
+        <h3 className="Sidebar-nav-item">About<span className="Sidebar-period">.</span></h3>
+        <h3 className="Sidebar-nav-item">Experience<span className="Sidebar-period">.</span></h3>
+        <h3 className="Sidebar-nav-item">Portfolio<span className="Sidebar-period">.</span></h3>
+        <h3 className="Sidebar-nav-item">Contact<span className="Sidebar-period">.</span></h3>
         <div className="Sidebar-resume"><h3 className="Sidebar-resume-text">Resume</h3></div>
       </div>
     </div>
