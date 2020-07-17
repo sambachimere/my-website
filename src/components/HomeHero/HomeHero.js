e9c212af74cc93bd9e79fdf5ebfeece937104a33
Samba Diaw <sambachimerediaw@gmail.com>
2020-07-17 17:08:05 +0200

    WIP Home Hero

---
+++ src/components/HomeHero/HomeHero.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './HomeHero.css';
+
+const HomeHero = () => {
+  return (
+    <div className="HomeHero">
+      <h3 className="HomeHero-hello">Hello, I am</h3>
+      <h1 className="HomeHero-myName">Samba Diaw,</h1>
+      <h1 className="HomeHero-iAm">Full Stack Web Developer.</h1>
+      <p className="HomeHero-quickPres">
+        Former SaaS integrator from Project Management to End-user Training and
+        Change Management, I know what makes great products, valuable to both
+        users and businesses.
+      </p>
+      <div className="HomeHero-callToActions">
+        <div className="HomeHero-contact">
+          <h3 className="HomeHero-contact-text">Resume</h3>
+        </div>
+        <div className="HomeHero-myWork">
+          <h3 className="HomeHero-myWork-text">See how I can help!</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeHero;
