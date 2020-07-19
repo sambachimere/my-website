e8cbad53d12ba06e9b7e386e90c0eb686e2c5a58
Samba Diaw <sambachimerediaw@gmail.com>
2020-07-19 14:56:46 +0200

    Done Portfolio Style

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import './Portfolio.css';
+import Serin from '../../assets/images/serin.png';
+import SelectAcademie from '../../assets/images/select-academie.png';
 import PortfolioItem from './PortfolioItem/PortfolioItem';
 
 const Portfolio = () => {
@@ -8,8 +10,26 @@ const Portfolio = () => {
     <div className="Portfolio">
       <h1 className="Portfolio-block-title">Portfolio</h1>
       <div className="Portfolio-container">
-        <PortfolioItem />
-        <PortfolioItem />
+        <PortfolioItem
+          image={Serin}
+          projectType="Website"
+          projectName="Site internet Serin."
+        />
+        <PortfolioItem
+          image={SelectAcademie}
+          projectType="Website"
+          projectName="Site internet Select Academie."
+        />
+        <PortfolioItem
+          image={SelectAcademie}
+          projectType="Website"
+          projectName="Site internet Select Academie."
+        />
+      </div>
+      <div className="Portfolio-dots">
+        <span className="Portfolio-dot"></span>
+        <span className="Portfolio-dot"></span>
+        <span className="Portfolio-dot"></span>
       </div>
     </div>
   );
--- src/components/Portfolio/PortfolioItem/PortfolioItem.js
+++ src/components/Portfolio/PortfolioItem/PortfolioItem.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './PortfolioItem.css';
 
-const PortfolioItem = () => {
+const PortfolioItem = (props) => {
   return (
     <div className="PortfolioItem">
-      <div className="PortfolioItem-image" />
+      <div
+        className="PortfolioItem-image"
+        style={{
+          backgroundImage: `url(${props.image})`,
+        }}
+      />
       <div className="PortfolioItem-yellowRectangle">
-        <h3 className="PortfolioItem-projectType">Website</h3>
+        <h3 className="PortfolioItem-projectType">{props.projectType}</h3>
       </div>
-      <h3 className="PortfolioItem-projectName">Site internet Serin.</h3>
+      <h3 className="PortfolioItem-projectName">{props.projectName}</h3>
     </div>
   );
 };
