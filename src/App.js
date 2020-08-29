f965dc5abb735970efc0c2b5ac5d23e990232874
Samba Diaw <sambachimerediaw@gmail.com>
2020-08-30 00:08:25 +0200

    To Delete

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Sidebar from './components/Sidebar/Sidebar';
+import HomeHero from './components/HomeHero/HomeHero';
 
 const App = () => {
-  return <div className="App"><Sidebar/></div>
+  return (
+    <div className="App">
+      <Sidebar/>
+      <HomeHero/>
+    </div>
+  )
 }
 
 export default App;
