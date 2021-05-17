bdba319fbc19f97912ce667d87bf044b3dfba7a6
Samba Diaw <sambadiaw@MacBook-Pro-de-Samba.local>
2021-05-17 13:44:44 +0000

    [Portfolio.js]: CRWN Shop -> user can now see the live app

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,15 +4,12 @@ import './Portfolio.css';
 
 // import Carousel from '../Carousel/Carousel';
 // import Serin from '../../assets/images/serin.png';
-// import SelectAcademie from '../../assets/images/select-academie.png';
 // import Toysly from '../../assets/images/toysly.png';
+// import Natours from '../../assets/images/natours.png';
+// import Forkify from '../../assets/images/forkify.png';
 import CrwnShop from '../../assets/images/crwn-shop.png';
-import Natours from '../../assets/images/natours.png';
-import Forkify from '../../assets/images/forkify.png';
 
 import { ReactComponent as CRWNLogo } from './../../assets/logos/crown-logo.svg';
-import NatoursLogo from './../../assets/logos/natours-logo.png';
-import ForkifyLogo from './../../assets/logos/forkify-logo.png';
 
 const Project1 = ({ 
   imageUrl, 
@@ -46,53 +43,55 @@ const Project1 = ({
           className="Portfolio-project-button" 
           style={{ marginBottom: marginBottom ? "100px" : "0px" }}
         >
-          <p className="Portfolio-project-button-text">{buttonTitle}</p>
+          <a href="https://crwn-s.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+            <p className="Portfolio-project-button-text">{buttonTitle}</p>
+          </a>
         </div>
       </div>
     </div>
   )
 }
 
-const Project2 = ({ 
-  children, 
-  imageUrl, 
-  name, 
-  description, 
-  marginBottom,
-  buttonTitle 
-}) => {
-  return (
-    <div className="Portfolio-project-block Portfolio-project-block-2">
-      <div 
-        className="Portfolio-project-description-block-2"
-        style={{ marginRight: '30px' }}
-      >
-        <div className="Portfolio-project-title-block">
-          { children }
-          <div className="Portfolio-project-inner">
-            <h3 className="Portfolio-project-title">{name}</h3>
-          </div>
-        </div>
-        <p className="Portfolio-project-description">{description}</p>
+// const Project2 = ({ 
+//   children, 
+//   imageUrl, 
+//   name, 
+//   description, 
+//   marginBottom,
+//   buttonTitle 
+// }) => {
+//   return (
+//     <div className="Portfolio-project-block Portfolio-project-block-2">
+//       <div 
+//         className="Portfolio-project-description-block-2"
+//         style={{ marginRight: '30px' }}
+//       >
+//         <div className="Portfolio-project-title-block">
+//           { children }
+//           <div className="Portfolio-project-inner">
+//             <h3 className="Portfolio-project-title">{name}</h3>
+//           </div>
+//         </div>
+//         <p className="Portfolio-project-description">{description}</p>
         
-        <div className="Portfolio-project-button">
-          <p className="Portfolio-project-button-text">{buttonTitle}</p>
-        </div>
-      </div>
+//         <div className="Portfolio-project-button">
+//           <p className="Portfolio-project-button-text">{buttonTitle}</p>
+//         </div>
+//       </div>
 
-      <div 
-        className="Portfolio-project-image-block"
-        style={{ marginBottom: marginBottom ? "100px" : "0px" }}
-      >
-        <img 
-          className="Portfolio-project-image" 
-          src={imageUrl} 
-          alt=""
-        />
-      </div>
-    </div>
-  )
-}
+//       <div 
+//         className="Portfolio-project-image-block"
+//         style={{ marginBottom: marginBottom ? "100px" : "0px" }}
+//       >
+//         <img 
+//           className="Portfolio-project-image" 
+//           src={imageUrl} 
+//           alt=""
+//         />
+//       </div>
+//     </div>
+//   )
+// }
 
 const Portfolio = () => {
   return (
@@ -103,58 +102,9 @@ const Portfolio = () => {
         name="CRWN Shop" 
         description="An e-commerce web application."
         marginBottom={true}
-        buttonTitle="VOIR LE SITE"
+        buttonTitle="LIVE APP"
       >
         <CRWNLogo className="Logo"/>
-      </Project1>
-
-      <Project2 
-        imageUrl={Natours}
-        name="Natours" 
-        description="A web application to book tours in nature."
-        marginBottom={true}
-        buttonTitle="VOIR LE SITE"
-      >
-        <img 
-          src={ NatoursLogo } 
-          style={{ width: '72px' }} 
-          className="Logo"
-          alt="" 
-        />
-      </Project2>
-
-      <div className="Project-Natours">
-        <Project1
-          imageUrl={Natours}
-          name="Natours" 
-          description="A web application to book tours in nature."
-          marginBottom={true}
-          buttonTitle="VOIR LE SITE"
-        >
-          <img 
-            src={ NatoursLogo } 
-            style={{ 
-              width: '50px',
-              marginTop: '12px  '
-            }}
-            className="Logo"
-            alt=""
-          />
-        </Project1>
-      </div>
-      
-      <Project1 
-        imageUrl={Forkify}
-        name="Forkify"
-        description="A web application to search for recipe."
-        buttonTitle="VOIR LE SITE"
-      >
-        <img 
-          src={ ForkifyLogo } 
-          style={{ width: '50px' }} 
-          className="Logo"
-          alt=""
-        />
       </Project1>
     </div>
   )
